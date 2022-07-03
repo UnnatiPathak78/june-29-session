7cd36a7d9b3e556019a074a7abdb3798ebc3b600
Unnati Pathak <unnatipathak78@gmail.com>
2022-07-03 07:16:29 +0530

    june-29-session

---
+++ index.js
@@ -0,0 +1,114 @@
+/*let num = 27;
+let counter = 1;
+let tick_count = 0;
+while(counter <= num){
+    if(num % counter == 0){
+        tick_count = tick_count + 1;
+    }
+
+counter = counter + 1;
+}
+
+if(tick_count == 2){
+    console.log("prime number");
+}
+else{
+    console.log("not prime number");
+}*/
+
+/*let my_array = [2, 4, 10, 40, 20, 18];
+let sum = 0;
+
+let idx = 0;
+while(idx < 6){
+    sum = sum + my_array[idx];
+    idx = idx + 1;
+}
+console.log(sum);
+*/
+
+
+/*let my_arr = [2, 3, 5, 6, 1, 2, 0];
+let max = my_arr[0];
+
+let idx = 0;
+while(idx < 7){
+    if(my_arr[idx] > max){
+        max =  my_arr[idx];
+    }
+idx = idx + 1;
+}
+console.log(max);
+*/
+
+
+/*let my_arr = [2, 3, 5, 6, 1, 2, 0];
+let max = my_arr[0];
+let idx = 0;
+while(idx < my_arr.length){
+    if(my_arr[idx] > max){
+        max =  my_arr[idx];
+    }
+idx = idx + 1;
+}
+console.log(max);
+*/
+
+
+/*let my_arr = [2, 3, 5, 6, 1, 2, 0, -19];
+let min = my_arr[0];
+for(let idx = 0; idx < my_arr.length; idx = idx + 1){
+    if(my_arr[idx] < min){
+        min = my_arr[idx];
+    }
+}
+console.log(min);
+*/
+
+
+/*let my_name = "UNNATI";
+for(idx = 0; idx < my_name.length; idx = idx + 1){
+    console.log(my_name[idx]);
+}
+*/
+
+/*let grade = "A";
+switch(grade){
+    case "A":
+    console.log("percent is more than 90");
+    break;
+    case "B":
+    console.log("percent is between 75% and 89.99%");
+    break;
+    case "C":
+    console.log("percent is between 60 and74.99");
+    break;
+    case "D":
+    console.log("Percent is between 40 and 59.99");
+    break;
+    default:
+    console.log("Invalid number");
+    break;
+
+}
+*/
+
+/*/let num = 10;
+let counter = 0;
+while(num >= 10){
+    
+    
+    counter = num + counter;
+    num = num - 1;
+    console.log(counter);
+}
+*/
+
+
+/*let sum = 0;
+for (let num = 1; num <= 10; num ++){
+    console.log(sum);
+    sum = sum + num;
+}
+*/
+
